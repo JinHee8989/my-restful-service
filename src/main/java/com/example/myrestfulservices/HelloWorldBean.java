ca08e118a35072559034f8bd019a6d5e68a3cf6f
jinheebyeon <wlsgml8989@gmail.com>
2022-01-03 23:45:02 +0900

    first

---
+++ src/main/java/com/example/myrestfulservices/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package com.example.myrestfulservices;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+}
