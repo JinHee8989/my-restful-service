8a38b862da133d25d4f5d9f70eae5370edf29d8b
JinHee8989 <wlsgml8989@gmail.com>
2022-01-05 23:01:50 +0900

    user service추가

--- src/main/java/com/example/myrestfulservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/example/myrestfulservices/helloworld/HelloWorldBean.java
@@ -1,10 +1,12 @@
-package com.example.myrestfulservices;
+package com.example.myrestfulservices.helloworld;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class HelloWorldBean {
     private String message;
 
---
+++ src/main/java/com/example/myrestfulservices/user/UserController.java
@@ -0,0 +1,27 @@
+package com.example.myrestfulservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service){ //생성자를 이용한 의존성 주입
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+
+}
