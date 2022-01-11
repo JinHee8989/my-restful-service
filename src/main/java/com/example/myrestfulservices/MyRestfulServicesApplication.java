2413f2c6e835cb7a516ab54af14dd4c2a3a2b3b6
jinheebyeon <wlsgml8989@gmail.com>
2022-01-11 23:31:36 +0900

    다국어 처리

--- src/main/java/com/example/myrestfulservices/MyRestfulServicesApplication.java
+++ src/main/java/com/example/myrestfulservices/MyRestfulServicesApplication.java
@@ -1,7 +1,12 @@
 package com.example.myrestfulservices;
 
+import com.sun.corba.se.spi.resolver.LocalResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class MyRestfulServicesApplication {
@@ -10,4 +15,11 @@ public class MyRestfulServicesApplication {
         SpringApplication.run(MyRestfulServicesApplication.class, args);
     }
 
+
+    @Bean //SpringBootApplication가 실행될때 해당 빈이 메모리에 올라가게됨
+    public SessionLocaleResolver localResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
