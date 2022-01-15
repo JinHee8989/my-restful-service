f23c9198900fea71a2a46919354f43a1ed926291
JinHee8989 <wlsgml8989@gmail.com>
2022-01-15 19:05:14 +0900

    uri로 api 버전관리

--- src/main/java/com/example/myrestfulservices/user/AdminUserController.java
+++ src/main/java/com/example/myrestfulservices/user/AdminUserController.java
@@ -3,6 +3,7 @@ package com.example.myrestfulservices.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -36,19 +37,16 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id){
+    @GetMapping("/v1/users/{id}")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null){
             throw new UserNotFoundException(String.format("ID[%s] not fount",id));
         }
 
-
         SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id","name","password","ssn");
 
-
         FilterProvider filters = new SimpleFilterProvider().addFilter("userInfo",filter); //어떤 빈을 대상으로 필터를 적용시킬지 정해줘야
-
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
         mapping.setFilters(filters);
@@ -56,5 +54,27 @@ public class AdminUserController {
         return mapping;
     }
 
+
+    @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not fount",id));
+        }
+
+        //user -> user2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user,userV2);
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id","name","password","grade");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("userInfoV2",filter); //어떤 빈을 대상으로 필터를 적용시킬지 정해줘야
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
 
 }
--- src/main/java/com/example/myrestfulservices/user/User.java
+++ src/main/java/com/example/myrestfulservices/user/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -13,6 +14,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password","ssn"}) //명명한 필드만 보여지지 않도록 함
 @JsonFilter("userInfo")
 public class User {
