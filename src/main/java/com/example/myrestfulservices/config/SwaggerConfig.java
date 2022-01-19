6e4f7f7fffb77e54d9b2886adb5eabccb331e7c6
JinHee8989 <wlsgml8989@gmail.com>
2022-01-19 23:12:28 +0900

    swagger 기능 추가

---
+++ src/main/java/com/example/myrestfulservices/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.example.myrestfulservices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){            //http://localhost:포트번호/swagger-ui.html, http://localhost:포트번호/v2/api-docs 에서 api document 확인 가능
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+
+}
