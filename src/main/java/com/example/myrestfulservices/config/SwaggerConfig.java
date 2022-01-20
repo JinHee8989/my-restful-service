8c87b896248a1a23f9b89e809a68eb31c0db3758
jinheebyeon <wlsgml8989@gmail.com>
2022-01-20 23:23:35 +0900

    swagger customizing 추가

--- src/main/java/com/example/myrestfulservices/config/SwaggerConfig.java
+++ src/main/java/com/example/myrestfulservices/config/SwaggerConfig.java
@@ -1,22 +1,33 @@
 package com.example.myrestfulservices.config;
 
+import org.jcp.xml.dsig.internal.dom.ApacheNodeSetData;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.client.LinkDiscoverer;
-import org.springframework.hateoas.client.LinkDiscoverers;
-import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final Contact DEFAULT_CONTACT = new Contact("Jinhee Byeon","http://www.joneconsulting.co.kr","wlsgml8989@gmail.com");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome Api Title","My User management REST API service","1.0","urn:tos",DEFAULT_CONTACT, "Apache 2.0","http://www.apache.org/license/LICENSE-2.0",new ArrayList<>());
+    private static  final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json","application/xml"));
+
     @Bean
     public Docket api(){            //http://localhost:포트번호/swagger-ui.html, http://localhost:포트번호/v2/api-docs 에서 api document 확인 가능
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
 }
