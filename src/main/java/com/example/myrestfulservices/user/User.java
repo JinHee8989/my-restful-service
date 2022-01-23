4646ac922fdf77d236ee0ce97fd44a85f094f7ed
jinheebyeon <wlsgml8989@gmail.com>
2022-01-24 00:54:54 +0900

    JPA 추가

--- src/main/java/com/example/myrestfulservices/user/User.java
+++ src/main/java/com/example/myrestfulservices/user/User.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -20,7 +23,11 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password","ssn"}) //명명한 필드만 보여지지 않도록 함
 //@JsonFilter("userInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") //swagger 커스터마이징에서 쓰임(객체 설명할 때)
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(90001,sysdate(),'user1','test1','890101-1010101');
+insert into user values(90002,sysdate(),'user2','test2','790101-2020202');
+insert into user values(90003,sysdate(),'user3','test3','990101-1111111');
